1d1d04d499c19dd325b4fb92e0172b3d136e3f1d
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-07-02 13:45:42 -0400

    Extracting Comment and SharePost components from MainArticle

--- src/MainArticle.js
+++ src/MainArticle.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Comments from './Comments'
+import SharePost from './SharePost'
+
 const MainArticle = () => {
    return (
          <div className="large-8 medium-12 columns article">
@@ -27,14 +30,8 @@ const MainArticle = () => {
               <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
             </section>
             <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
+            <Comments />
+            <SharePost />
             </div>
           </div>
    )
